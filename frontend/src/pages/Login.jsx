dbc2e3371bc0ed02f0188a4b1b3155789a320e18
Ioannis Metzakis <johnmetzakis@gmail.com>
2026-03-22 17:26:35 +0200

    Final changes. READY FOR DEPLOYMENT

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -21,7 +21,7 @@ export default function LoginPage() {
       }
 
       localStorage.setItem("token", res.data.token);
-      navigate("/watched");
+      navigate("/");
     } catch (err) {
         if (err.response && err.response.data) {
             const backendError = err.response.data.error || err.response.data.message;
