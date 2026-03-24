fc01a8133181f4d2222cda74dbc043dd729427ea
Ioannis Metzakis <johnmetzakis@gmail.com>
2026-03-24 17:16:49 +0200

    Added SpeedInsights for vercel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
